6201e2b1564dc2a70fdc90d0e5a04449c5d68387
maxdanko2000 <maxdanko12345@yandex.ru>
2022-04-27 19:01:33 +0300

    feat: some fixes

--- src/js/includes/journey-video-slider.js
+++ src/js/includes/journey-video-slider.js
@@ -1,6 +1,22 @@
-// const swiper = new Swiper('.swiper', {
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev'
-//     },
-// });
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    direction: 'horizontal',
+    loop: true,
+
+
+    pagination: {
+        el: '.slider-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
