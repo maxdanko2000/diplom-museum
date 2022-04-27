b79bb5621f8b7f6dc28d1243a59a78a65d36f097
maxdanko2000 <maxdanko12345@yandex.ru>
2022-04-27 15:47:42 +0300

    feat: work with adaptive

--- src/js/includes/explore-comparison.js
+++ src/js/includes/explore-comparison.js
@@ -1,6 +1,7 @@
 const divisor = document.querySelector(".explore__comparison-img-after");
-const comparisonWidth = document.querySelector(".explore__comparison").clientWidth;
+
 const comparison = document.querySelector(".explore__comparison");
+const comparisonWidth = comparison.clientWidth;
 
 comparison.addEventListener("mousemove", (e) => {
     divisor.style.width = (e.offsetX * 100 / comparisonWidth) + "%";
