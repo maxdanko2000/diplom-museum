6ed487039729c2ab2728ea30437603112e0f2878
maxdanko2000 <maxdanko12345@yandex.ru>
2022-05-04 21:42:34 +0300

    feat: some fixes

--- src/js/includes/explore-comparison.js
+++ src/js/includes/explore-comparison.js
@@ -1,6 +1,6 @@
 const divisor = document.querySelector(".explore__comparison-img-after");
 const comparison = document.querySelector(".explore__comparison");
-const comparisonWidth = comparison.clientWidth;
+const comparisonWidth = document.querySelector(".explore__comparison").clientWidth;
 
 comparison.addEventListener("mousemove", (e) => {
     divisor.style.width = (e.offsetX * 100 / comparisonWidth) + "%";
