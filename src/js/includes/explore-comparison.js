0518b8f1f1ac007c5b5bcf50656e686487b629e1
maxdanko2000 <maxdanko12345@yandex.ru>
2022-05-05 09:23:09 +0300

    feat: some fixes

--- src/js/includes/explore-comparison.js
+++ src/js/includes/explore-comparison.js
@@ -3,7 +3,7 @@ const divisor = document.querySelector(".explore__comparison-divisor");
 function comparing() {
     let slideValue = document.querySelector(".explore__comparison-divisor").value;
     document.querySelector(".explore__comparison-after").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
-    console.log(slideValue);
+
 }
 
 
