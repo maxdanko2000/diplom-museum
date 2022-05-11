9062badbe6a3e6dfab286b2913f641eb753438d7
maxdanko2000 <maxdanko12345@yandex.ru>
2022-05-11 16:59:00 +0300

    feat: add form and user page

--- src/js/includes/journey-video-slider.js
+++ src/js/includes/journey-video-slider.js
@@ -1,15 +1,38 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from "swiper";
 
-const swiper = new Swiper('.journey-slider-inner', {
+const swiper = new Swiper(".journey-slider-inner", {
     modules: [Navigation, Pagination],
-    direction: 'horizontal',
-    spaceBetween: 40,
+    direction: "horizontal",
+    slidesPerView: 4,
+    spaceBetween: 80,
     pagination: {
-        el: '.journey-slider__pagination',
-        clickable: true
+        el: ".journey-slider__pagination",
+        clickable: true,
     },
     navigation: {
-        nextEl: '.journey-slider__btn-next',
-        prevEl: '.journey-slider__btn-prev',
+        nextEl: ".journey-slider__btn-next",
+        prevEl: ".journey-slider__btn-prev",
     },
+    // breakpoints: {
+    //     1200: {
+    //         slidesPerView: 4,
+    //         spaceBetween: 80
+    //     },
+    //     992: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 120
+    //     },
+    //     768: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 160
+    //     },
+    //     // 480: {
+    //     //     slidesPerView: 2,
+    //     //     spaceBetween: 160
+    //     // },
+    //     // 375: {
+    //     //     slidesPerView: 1,
+    //     //     spaceBetween: 120
+    //     // }
+    // }
 });
