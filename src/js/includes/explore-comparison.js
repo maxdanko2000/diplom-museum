ef41b98992dafcf096216898ddcbcd656b381602
maxdanko2000 <maxdanko12345@yandex.ru>
2022-05-18 20:07:18 +0300

    feat: some fixes

--- src/js/includes/explore-comparison.js
+++ src/js/includes/explore-comparison.js
@@ -1,11 +1,15 @@
-const afterImg = document.querySelector(".explore__comparison-after");
 export class Comparison {
-  constructor() {}
-  compare() {
-    return () => {
+  constructor() {
+    this.comparing();
+  }
+  comparing() {
+    const divisor = document.querySelector(".explore__comparison-divisor");
+    divisor.addEventListener("input", () => {
       let slideValue = document.querySelector(".explore__comparison-divisor").value;
-      afterImg.style.clipPath = `polygon(0 0,${slideValue}% 0, ${slideValue}% 100%, 0 100%`;
-    };
+      const afterImg = (document.querySelector(
+        ".explore__comparison-after"
+      ).style.clipPath = `polygon(0 0,${slideValue}% 0, ${slideValue}% 100%, 0 100%`);
+      return afterImg;
+    });
   }
 }
-// function compare() {}
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
-// import "./includes/explore-comparison.js";
-// import "./includes/journey-video-slider.js";
-// import "./includes/hero-slider.js";
-import { Comparison } from "./includes/explore-comparison.js";
+import "./includes/journey-video-slider.js";
+import "./includes/hero-slider.js";
 
-const divisor = document.querySelector(".explore__comparison-divisor");
-const comparison = new Comparison();
-divisor.addEventListener("input", comparison.compare());
+import { Comparison } from "./includes/explore-comparison";
+
+let comparison = new Comparison();
+comparison.comparing();
