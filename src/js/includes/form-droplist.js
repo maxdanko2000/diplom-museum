f271dff5e9d7d839ef9ec65177cf173751d46dc4
maxdanko2000 <maxdanko12345@yandex.ru>
2022-05-19 18:22:34 +0300

    feat: some fixes

---
+++ src/js/includes/form-droplist.js
@@ -0,0 +1,7 @@
+const openListBtns = document.querySelectorAll(".droplist-toggler");
+const lists = document.querySelectorAll(".tickets-form__droplist");
+const listItem = document.querySelectorAll(".tickets-form__droplist-item");
+
+for (const openListBtn of openListBtns) {
+  openListBtn.addEventListener("click", () => {});
+}
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ import { heroOptions } from "./includes/hero-slider.js";
 import { Comparison } from "./includes/explore-comparison";
 import { journeyOptions } from "./includes/journey-video-slider.js";
 
+import "./includes/form-droplist.js";
+
 new Comparison().comparing();
 new Swiper(".hero-slider", Object.assign(heroOptions));
 new Swiper(".journey-slider-inner", Object.assign(journeyOptions));
